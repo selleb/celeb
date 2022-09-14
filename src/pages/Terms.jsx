574809e8840ce98af9facae4025808052b5f520b
selleb <113495004+selleb@users.noreply.github.com>
2022-09-14 08:13:45 +0100

    terms

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -1,40 +1,72 @@
 import React from 'react'
+import './styles/terms.css'
 
 const Terms = () => {
     return (
         <div>
-            <h1>Terms of Service</h1>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro a nostrum id non illum cupiditate.
+            <h1>PRIVACY POLICY</h1>
+This Privacy Policy (Policy) sets forth the policies regarding the collection and use of information relating to the All American Speakers website located at allamericanspeakers.com (Website) as well as device-based applications (Apps), which are operated by New Age Media Ventures LLC d/b/a All American Entertainment (AAE, All American Speakers, the company, we, or us).
 
-            <h1>Limited License</h1>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates, sapiente qui nulla quaerat incidunt nam.
+<h1>PERSONAL INFORMATION WE COLLECT AND HOW WE USE IT</h1>
+Personal Information means information that can be used to identify or contact you, such as your name, email address, telephone number or similar data. We only collect personal information that you choose to share with us, including but not limited to contacting us by filling out a contact or booking request form on the website, creating a personal account on the site, sending in a customer feedback survey, or signing up for notifications or newsletters.
 
-            <h1>Copyrights and Trademarks</h1>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia ratione voluptas exercitationem autem iusto minus.
+<h1>USE OF PERSONAL INFORMATION</h1>
+We may use your Personal Information to provide you with the materials you've requested and/or purchased, to send periodic marketing or promotional notices to you, or to notify you of any changes to this Policy. We may also use your Personal Information to improve our marketing efforts, statistically evaluate site usage, improve or customize Website content, layout and services. You can always opt-out of receiving these communications from us by following the "opt-out" procedures described below.
 
-            <h1>Links to Third-Party Websites</h1>
-            Our Website contains links to Websites owned or operated by parties other than HTML CSS JS. Such links are provided for your reference only. HTML CSS JS does not monitor or control outside Websites and is not responsible for their content. HTML CSS JS’s inclusion of links to an outside Website does not imply any endorsement of the material on our Website or, unless expressly disclosed otherwise, any sponsorship, affiliation or association with its owner, operator or sponsor, nor does HTML CSS JS’ inclusion of the links imply that HTML CSS JS is authorized to use any trade name, trademark, logo, legal or official seal, or copyrighted symbol that may be reflected in the linked Website.
+<h1>GENERAL</h1>
+Any information you provide to us must be true, accurate, current, non-misleading, and consistent and relevant for the purpose for which you are providing the information.
 
-            <h1>Content Disclaimer</h1>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum quas sit facere illo soluta veniam!
+If you contact us via the Website or Apps, we will only use your Personal Information contained in your email to respond to your specific inquiry or comments in the email. We may also retain your email for purposes other than responding to your email.
 
-            <h1>Indemnification</h1>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, temporibus blanditiis quo odio pariatur repellat?
+<h1>USAGE DATA</h1>
+The Website and Apps may automatically track certain information about your behavior while visiting the Website. The information gathered includes: The URL from which you accessed our Website, which URL you visit next, and the search terms you used to arrive at our Website. We may collect and store this aggregate information in order to measure the Website's performance and improve its design and functionality.
 
-            <h1>Privacy Policy</h1>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur quo voluptas, id recusandae atque obcaecati.
+We might collect data from the content you view, share via social networks, and like or dislike via the corresponding buttons on the Website or Apps in order to better understand your preferences so that we can create customized content.
 
-            <h1>Your Privacy</h1>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta nemo, totam sit molestiae architecto obcaecati?
+We also aggregate video viewing information with high-level demographics such as age, gender, and locale and provide those high-level demographics to third parties, including advertisers and advertising partners. We may also share this aggregate information with third parties to illustrate how the Website and Apps are used, to improve their functionality and effectiveness, as well as for other marketing purposes. However, we do NOT share personally identifiable information such as name, address, or birthdate with any third parties.
 
-            <h1>Definitions</h1>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet necessitatibus commodi saepe deserunt, voluptate ex.
+<h1>SOCIAL MEDIA INTEGRATION</h1>
+The Website and Apps include the ability to link your user account to your social network accounts, at your direction. We retrieve information, such as "likes", "subscriptions", "friend lists", and "posts" to improve content recommendations. We do NOT share this information with advertisers or sell the information to any third parties, but we reserve the right to share aggregated information about our audience (with NO reference to personally identifiable information such as name, address, or birthdate).
 
-            <h1>Cookies</h1>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quod itaque doloremque quo voluptas temporibus.
+<h1>COOKIES</h1>
+The Website uses both "persistent cookies" and "session cookies," which is information stored in a browser's memory and passed back to the applicable server whenever a request for a new page on the Website is made.
 
-            <h1>Advertising</h1>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates amet sit consequuntur sunt rerum accusamus.
+<h1>USE AND DISCLOSURE</h1>
+Subject to applicable law, we and our Service Providers (as described below) may disclose your Personal Information:
+
+To entities affiliated with AAE and/or New Age Media Ventures LLC (our parent company);
+To unaffiliated third parties that are under contract to perform services for or on behalf of AAE (Service Providers), and are required to uphold and maintain AAE's policies with respect to privacy and the treatment of your Personal Information (e.g., vendors maintaining our Website and Apps or computer databases or performing fulfillment or marketing activities for AAE and/or New Age Media Ventures LLC);
+To other persons as permitted by applicable law or regulation; and
+To law enforcement personnel and agencies, or as part of a legal process, in order to protect our property or in furtherance of an investigation regarding a breach of the Website rules and policies, unauthorized access to or use of the Website or any other illegal activities.
+Any access to such information will be limited to the purpose for which such information was provided to our affiliate, subsidiary or Service Provider. Our affiliates, subsidiaries and Service Providers may be located throughout the world. Accordingly, information may be sent worldwide to countries that have a different level of privacy protection than the United States.
+
+<h1>CONSENT TO PROCESSING</h1>
+By using the Website and/or Apps, or providing information to AAE, you understand and unambiguously consent to the collection, processing, transfer, and use of information in the United States and other countries and territories, for the purposes set forth in this Policy. You hereby consent to the collection, processing and disclosure by AAE of your Personal Information as described under this Policy, including but not limited to the transfer of your Personal Information between AAE's Service Providers, affiliates, employees, and subsidiaries in accordance with this Policy. For further certainty, any consent relating to a right to transfer information referred to in this paragraph shall be deemed to include your consent to the transfer of the applicable Personal Information to a jurisdiction which may have a different level of privacy protection than that available in your own country.
+
+<h1>OPT-OUT CHOICES</h1>
+To "opt-out" of (1) receiving communications from AAE or New Age Media Ventures LLC and its subsidiaries (except for communications necessary for your use of the Website or Apps), or (2) any other consent previously granted for a specific purpose concerning your Personal Information, send an e-mail to AAE at: info@allamericanspeakers.com or – if available – use the opt-out link provided in the communication you received.
+
+<h1>REVIEWING OR CHANGING INFORMATION</h1>
+If you have provided Personal Information to us, you may request to review, change or delete your information previously provided to us by sending an e-mail to info@allamericanspeakers.com. Requests for changes to or deletions of your Personal Information should be made by you directly. If so requested, you will need to be able to provide proof of your identity (e.g. name, email address).
+
+<h1>OTHER WEB SITES</h1>
+This Policy applies to all sites under the domains allamericanspeakers.com and allamericanentertainment.com. The reference to our “Website” includes links to other web sites, including access to content, products and services of such sites (Other Sites). In some cases, these Other Sites are made available via "framing technology", and may appear to be part of this Website. Although some of the entities controlling these linked or framed sites may be under contract with AAE, we urge you to familiarize yourself with the individual privacy and other terms for each linked or framed site prior to submitting your Personal Information.
+
+<h1>SALE OR TRANSFER OF INFORMATION</h1>
+In the event of a sale, merger, liquidation, dissolution, or transfer of substantially all of the assets of AAE, Personal Information and/or usage data collected about you from the Website may be sold, assigned, or transferred to the party acquiring all or substantially all of the equity and/or assets of AAE in order to permit the party to continue the operation of the Website. Subject to your ability to opt-out as described above, by using this Website and submitting your information to us, you consent to the sale and transfer of your information as described in this paragraph.
+
+<h1>SECURITY</h1>
+AAE takes seriously the security of the information it collects. AAE has therefore implemented technology and security policies and procedures intended to reduce the risk of accidental destruction or loss, or the unauthorized disclosure or access to such information, reasonably appropriate to the nature of the data concerned.
+
+<h1>UPDATES TO PRIVACY POLICY</h1>
+AAE may, in its sole discretion, update this Policy at any time and from time to time, by posting the amended Policy on the Website and/or Apps. The amended Policy shall be effective upon posting, and your access to or use of the Website or Apps following an update shall be deemed consent to the revised Policy. If you have provided Personal Information to us, amendments to this Policy may (but are not required to) be sent to you via email or such other form of communication as you have requested. We urge each visitor to refer back to this page regularly and especially prior to providing or updating any Personal Information. This Policy may not be otherwise amended without the written consent of AAE. This Policy was last updated on November 14, 2017.
+
+<h1>CHILDREN</h1>
+Our Website is not generally intended for use by children under the age of 13, and AAE does not knowingly collect personally identifiable information from children in this age group. If we become aware that a user under the age of 13 has registered with the Website or otherwise provided Personal Information to us, or is attempting to do so, we will notify the user that he or she is not eligible for registration or to submit Personal Information. We will then expunge any of his or her personally identifiable information from our records.
+
+<h1>CONTACT, QUESTIONS, COMMENTS, COMPLAINTS</h1>
+If you would like to communicate with AAE regarding privacy issues or have questions, comments or complaints about this Policy or the Website, please forward such communication to: info@allamericanspeakers.com.
+
 
         </div>
     )
